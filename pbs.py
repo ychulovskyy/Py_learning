4c9232366c4f3639dfc10d63a1846efad2c4e902
Iurii Chulovskyi <yuriy.chulovskyy@gmail.com>
2019-12-10 20:34:49 +0100

    Rename dict to record to avoid shadowing of Built-in Functions https://docs.python.org/3/library/functions.html

--- pbs.py
+++ pbs.py
@@ -2,14 +2,14 @@ import csv
 
 nmr = 'boB'
 lst_U = []
-dict = {}
+record = {}
 
 with open('pb.csv', 'r') as fl:
     rds = csv.DictReader(fl)
-    for dict in rds:  # All readings from the file go to Dictionary 'dict'
-        l_fname = dict.get('f_name')
-        l_lname = dict.get('l_name')
-        l_number = dict.get('number')
+    for record in rds:  # All readings from the file go to Dictionary 'dict'
+        l_fname = record.get('f_name')
+        l_lname = record.get('l_name')
+        l_number = record.get('number')
         for i in range(len(l_fname) - 1):  # The first name analysis
             if l_fname[i:i + len(nmr)].lower() == nmr.lower() and not l_number in lst_U:
                 lst_U.append(l_number)  # adding the number to lst_U  if we found something
@@ -23,7 +23,7 @@ print('We found...')
 
 with open('pb.csv', 'r') as fl:
     rds = csv.DictReader(fl)
-    for dict in rds:  # comparing file-data and the list of found numbers
-        if dict.get('number') in lst_U:
-            print('Name:', dict.get('f_name'), dict.get('l_name'), '     Phone number:', dict.get('number'))
+    for record in rds:  # comparing file-data and the list of found numbers
+        if record.get('number') in lst_U:
+            print('Name:', record.get('f_name'), record.get('l_name'), '     Phone number:', record.get('number'))
 if lst_U == []: print('Nothing')
